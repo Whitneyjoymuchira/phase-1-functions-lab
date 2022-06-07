51df5ad8651b4cfaea01c083290c6c4e77e351f3
Whitney Muchira <whitneymuchira@gmail.com>
2022-06-07 23:59:21 +0300

    commit tree ebd663eebca4bfecde0a87f3885dadfea2285644

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+
+        function distanceFromHqInBlocks(someValue){
+        someValue = someValue - 42;
+         return Math.abs(someValue)
+       
+
+   }
+   function distanceFromHqInFeet(someValue) {
+   return  distanceFromHqInBlocks(someValue) *264;
+   }
+
+   function distanceTravelledInFeet(start, destination) {
+    return distanceFromHqInFeet (destination)-distanceFromHqInFeet (start);
+  }
+  
+  
+  
+  function calculatesFarePrice(start, destination) {
+    
+    var dist = distanceTravelledInFeet(start, destination)
+    if (dist < 400) {
+      return 0
+    }
+    else if (dist >= 400 && dist<= 2000) {
+      return (dist -  400)* 0.02
+    }
+    else if(dist> 2000 && dist< 2500) {
+      return 25
+    }
+    else if(dist > 2500){
+      return 'cannot travel that far';
+    }
+  
+  }
+  
